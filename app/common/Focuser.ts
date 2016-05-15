13183a6a8628a1775f6fd104b8cfc836378b9334
Kenton Gray <kentongray@gmail.com>
2016-05-14 20:40:39 -0500

    hack way of autofocussing

---
+++ app/common/Focuser.ts
@@ -0,0 +1,15 @@
+import {Directive, Renderer, ElementRef} from "angular2/core";
+@Directive({
+  selector : '[focuser]'
+})
+export default class Focuser {
+  constructor(public renderer: Renderer, public elementRef: ElementRef) {}
+
+  ngOnInit() {
+    const searchInput = this.elementRef.nativeElement.querySelector('input');
+    setTimeout(() => {
+      //delay required or ionic styling gets finicky
+      this.renderer.invokeElementMethod(searchInput, 'focus', []);
+    }, 0);
+  }
+}
