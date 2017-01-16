2d13a5a386e914d5dff498ab71a303f6acc1a418
VictorCoding <victor.ramos1@gmail.com>
2017-01-16 00:00:32 -0600

    [localization] localized more of the text found in the home page that was being populated by the component class
    - add new keys to english and spanish localization files
    - localize date pipe
    - delete date filter from HomePage component

--- src/pages/home/HomePage.ts
+++ src/pages/home/HomePage.ts
@@ -8,12 +8,12 @@ import {Component, ViewChild} from "@angular/core";
 import {rejectFirst, HandledPromiseError} from "../../common/PromiseExceptionHandler";
 import {DetailPage} from "../detail/DetailPage";
 import {UrlUtil} from "../../common/UrlUtil";
+import {TranslateService} from "ng2-translate";
 
 @Component({
   templateUrl: 'HomePage.html'
 })
 export class HomePage {
-  public static readonly ERROR_LOADING = 'We Had a Problem Loading Your Schedule. \nThe City of Houston may be having issues';
   @ViewChild(Content) content: Content;
   private coords;
   private geolocation:any;
@@ -28,6 +28,7 @@ export class HomePage {
   loadingContent:Loading;
   loading:boolean;
   currentSearch:string;
+  translatedText = {};
 
   constructor(
       public toastCtrl: ToastController,
@@ -35,20 +36,50 @@ export class HomePage {
       private platform:Platform,
       private nav:NavController,
       private SchedulerService:Scheduler,
-      addressLookup:AddressLookup
+      addressLookup:AddressLookup,
+      translate:TranslateService
   ) {
     this.moment = moment;
     this.geolocation = Geolocation;
     this.addressLookup = addressLookup;
     this.announceUpdates();
     this.loadEvents();
+    translate.get([
+      'We_Had_A_Problem_Loading_Your_Schedule_The_City_Of_Houston_May_Be_Having_Issues',
+      'You_Can_Now_Tap_The_Types_Of_Trash_To_Learn_More',
+      'Looking_Up_Coordinates',
+      'Unable_To_Locate_You',
+      'Looking_Up_Your_Schedule',
+      'Error_Loading_Events',
+      'Starting_Up',
+      'Finding_Your_Location',
+      'We_Couldnt_Look_Up_Your_Location_Check_Your_Location_Permissions',
+      'Something_Went_Wrong',
+      'Error_Finding_Position',
+      'Try_Again',
+      'One_Sec',
+    ]).subscribe(res => {
+      this.translatedText['errorLoading'] = res['We_Had_A_Problem_Loading_Your_Schedule_The_City_Of_Houston_May_Be_Having_Issues'];
+      this.translatedText['youCanNowTapTheTypesOfTrashToLearnMore'] = res['You_Can_Now_Tap_The_Types_Of_Trash_To_Learn_More'];
+      this.translatedText['lookingUpCoordinates'] = res['Looking_Up_Coordinates'];
+      this.translatedText['unableToLocateYou'] = res['Unable_To_Locate_You'];
+      this.translatedText['lookingUpYourSchedule'] = res['Looking_Up_Your_Schedule'];
+      this.translatedText['errorLoadingEvents'] = res['Error_Loading_Events'];
+      this.translatedText['startingUp'] = res['Starting_Up'];
+      this.translatedText['findingYourLocation'] = res['Finding_Your_Location'];
+      this.translatedText['weCouldntLookUpYourLocationCheckYourLocationPermissions'] = res['We_Couldnt_Look_Up_Your_Location_Check_Your_Location_Permissions'];
+      this.translatedText['somethingWentWrong'] = res['Something_Went_Wrong'];
+      this.translatedText['errorFindingPosition'] = res['Error_Finding_Position'];
+      this.translatedText['tryAgain'] = res['Try_Again'];
+      this.translatedText['oneSec'] = res['One_Sec'];
+    });
   }
 
   announceUpdates() {
     if(!window.localStorage.getItem('announcedTapTypes')) {
       window.localStorage.setItem('announcedTapTypes', true.toString());
       let toast = this.toastCtrl.create({
-        message: 'You can now tap the types of trash to learn more!',
+        message: this.translatedText['youCanNowTapTheTypesOfTrashToLearnMore'],
         showCloseButton: true,
         duration: 20000,
       });
@@ -73,16 +104,16 @@ export class HomePage {
   selectAddress(suggestion) {
     this.searching = false;
     this.addresses = null;
-    this.showLoader('Looking up Coordinates');
+    this.showLoader(this.translatedText['lookingUpCoordinates']);
     this.addressLookup.lookupCoordinates(suggestion)
       .then(r => {
-        this.showLoader('Looking up Schedule');
+        this.showLoader(this.translatedText['lookingUpYourSchedule']);
         return this.loadEventsForPosition(r);
-      }, e => this.showError("Unable to Locate You"))
+      }, e => this.showError(this.translatedText['unableToLocateYou']))
       .then(r => {
         this.hideLoader();
         return r;
-      }, e => this.showError("Error Loading Events"))
+      }, e => this.showError(this.translatedText['errorLoadingEvents']))
       .catch(e => {
         this.hideLoader();
         throw "Error Loading Events";
@@ -122,21 +153,11 @@ export class HomePage {
     });
   }
 
-  static dateFilter(day) {
-    if (moment().isSame(day, 'day')) {
-      return 'Today ' + day.format('MMM Do');
-    } else if (moment().add(1, 'days').isSame(day, 'day')) {
-      return 'Tomorrow ' + day.format('MMM Do');
-    } else {
-      return day.format('dddd MMM Do');
-    }
-  }
-
   static dayOfWeek(day) {
     return moment().day(day).format("dddd");
   }
 
-  showLoader(str = 'One Sec!') {
+  showLoader(str = this.translatedText['oneSec']) {
     if (!this.loadingContent) {
       this.loadingContent = this.loadingController.create({content: str});
     }
@@ -158,17 +179,17 @@ export class HomePage {
   }
 
   loadEvents() {
-    this.showLoader('Starting Up!');
+    this.showLoader(this.translatedText['startingUp']);
     return this.platform.ready().then(() => {
-      this.showLoader('Finding Your Location');
+      this.showLoader(this.translatedText['findingYourLocation']);
       return this.geolocation.getCurrentPosition()
         .then(pos => {
-          this.showLoader('Looking Up Your Schedule!');
+          this.showLoader(this.translatedText['lookingUpSchedule']);
           return pos;
         })
-        .catch(rejectFirst('We couldn\'t look up your location.\n Check Your Location Permissions'))
+        .catch(rejectFirst(this.translatedText['weCouldntLookUpYourLocationCheckYourLocationPermissions']))
         .then(this.loadEventsForPosition.bind(this))
-        .catch(rejectFirst(HomePage.ERROR_LOADING));
+        .catch(rejectFirst(this.translatedText['errorLoading']));
     }).then(this.hideLoader.bind(this), this.promiseCatcher);
   }
 
@@ -180,14 +201,14 @@ export class HomePage {
         this.showError(error.message)
       }
       else {
-        this.showError('Something Went Wrong...');
+        this.showError(this.translatedText['somethingWentWrong']);
       }
       this.hideLoader.bind(this)
   };
 
 
   errorFindingPosition(err) {
-    this.showError('Error Finding Position');
+    this.showError(this.translatedText['errorFindingPosition']);
     console.error('Error Finding Position', err);
   }
 
@@ -203,7 +224,7 @@ export class HomePage {
   }
 
   retry():Promise<Array<any>> {
-    this.showLoader('Trying again');
+    this.showLoader(this.translatedText['tryAgain']);
     this.clearError();
     console.log('reloading', this.coords);
     if(!this.coords) {
@@ -213,7 +234,7 @@ export class HomePage {
     else {
       //they might have put in a custom address so try to look up the same ones
       return this.loadEventsForPosition({coords: this.coords})
-        .catch(rejectFirst(HomePage.ERROR_LOADING))
+        .catch(rejectFirst(this.translatedText['errorLoading']))
         .then(r => {
           this.hideLoader();
           return r
