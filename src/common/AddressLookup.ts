f290f78d2ca7115e25d66e518da7b478107d0d9a
kentongray <kentongray@gmail.com>
2020-10-20 14:49:55 -0500

    Update AddressLookup.ts

--- src/common/AddressLookup.ts
+++ src/common/AddressLookup.ts
@@ -49,7 +49,7 @@ export class AddressLookup {
     if (!suggestion) {
       return Promise.reject(null);
     }
-    return this.$http.get(`http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/findAddressCandidates?&Address=${suggestion.text}&State=TX&f=json&City=Houston&maxLocations=10&maxResultSize=1&outFields=StreetType&magicKey=${suggestion.magicKey}&category=&location=-95.3632700,29.7632800&distance=10000&f=pjson`)
+    return this.$http.get(`https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/findAddressCandidates?&Address=${suggestion.text}&State=TX&f=json&City=Houston&maxLocations=10&maxResultSize=1&outFields=StreetType&magicKey=${suggestion.magicKey}&category=&location=-95.3632700,29.7632800&distance=10000&f=pjson`)
       .map(res => res.json())
       .toPromise()
       .then((r: { candidates: FindAddressCandidatesResult[] }) => {
